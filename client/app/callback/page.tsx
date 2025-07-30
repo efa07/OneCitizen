aef83dd8842a2cf959dc0109505a9b63aae62f14
Efa Tariku <efatariku07@gmail.com>
2025-07-30 14:17:37 +0300

    update

--- client/app/callback/page.tsx
+++ client/app/callback/page.tsx
@@ -1,5 +1,11 @@
+'use client';
 import Callback from '../../components/Callback';
+import { Suspense } from 'react';
 
 export default function CallbackPage() {
-  return <Callback />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Callback />
+    </Suspense>
+  );
 }
